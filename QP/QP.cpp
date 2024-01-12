b983bdc31103770aa75af606dbb9ad9e2a1aadc4
adelinazdes <adelina.ch15@mail.com>
2024-01-13 02:54:22 +0300

    2 задачи из блока 2 Классическая задача PP поверх метода Эйлера Классическая задача PP поверх Эйлера на методе Ньютона

--- QP/QP.cpp
+++ QP/QP.cpp
@@ -67,7 +67,12 @@ struct pipe
     double relative_roughness, Re;
 };
 void u_Newton(pipe& myPipe);
-
+void u_Newton_EULER(pipe& myPipe);
+double pressure_EULER(pipe& myPipe);
+struct EULER
+{
+    double p_0_rachet;
+};
 int main()
 {
     
@@ -98,9 +103,18 @@ int main()
     myPipe.t_w = myPipe.lambda / 8 * myPipe.ro * pow(myPipe.V, 2);
 
     //double p_L = pressure_EULER(myPipe);   //присваиваем значения из функции pressure
+   
     u_Newton(myPipe);
-
+    u_Newton_EULER(myPipe);
+    
 };
+
+
+
+
+
+
+
 
 class QP_Newton : public fixed_system_t<1>
 {
@@ -122,19 +136,18 @@ using fixed_system_t<1>::var_type; public:
         double lambda = hydraulic_resistance_isaev(pipe_dannye.Re, pipe_dannye.relative_roughness);
         double p_L = (pipe_dannye.p_0 / (pipe_dannye.ro * M_G) - pipe_dannye.z_0 + pipe_dannye.z_L - pipe_dannye.lambda * (pipe_dannye.L / pipe_dannye.D) * pow(v, 2) / (2 * M_G)) * (pipe_dannye.ro * M_G);
         double delta_p_L;
-        
 
-            return
+
+        return
         {
 
-           delta_p_L =  pipe_dannye.p_L- p_L
+           delta_p_L = pipe_dannye.p_L - p_L
 
         };
     };
 };
 
 void u_Newton(struct pipe& myPipe) {
-
 
     // Подключение бибилиотеки
 // Класс, для системы размерности <2> - Векторный случай
@@ -149,9 +162,91 @@ void u_Newton(struct pipe& myPipe) {
     fixed_solver_result_t<1> result;
     // Решение системы нелинейныйх уравнений <1> с помощью решателя Ньютона - Рафсона
     // { 0, 0 } - Начальное приближение
-    fixed_newton_raphson<1>::solve_dense(test, {1}, parameters, &result);
-    cout << "u = " << result.argument;
+    fixed_newton_raphson<1>::solve_dense(test, { 1 }, parameters, &result);
+    cout << '\n' << "Скорость изначальная " << '\n' << "u = " << myPipe.V << '\n' << "Классическая задача PP поверх метода Эйлера " << '\n' << "u = " << result.argument;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class QP_Newton_EULER : public fixed_system_t<1>
+{
+    /// @brief Ссылка на структуру с параметрами трубы 
+    const  pipe& pipe_dannye;
+    
+using fixed_system_t<1>::var_type; public:
+    /// @brief Констуктор
+    /// @param pipe Ссылка на сущность трубы 
+    /// @param problem Ссылка на сущность с условием задачи
+    QP_Newton_EULER(const pipe& pipe_dannye) : pipe_dannye{ pipe_dannye }{};
+
+    /// @brief Функция невязок - все члены уравнения Бернулли в правой части 
+    /// @param v - скорость течения нефти, [м/с] 
+    /// @return Значение функции невязок при заданной скорости 
+    var_type residuals(const var_type& v)
+    { //v - искомая скорость
+        double Re = v * pipe_dannye.D / pipe_dannye.u;
+        double lambda = hydraulic_resistance_isaev(pipe_dannye.Re, pipe_dannye.relative_roughness);
+        double t_w = lambda / 8 * pipe_dannye.ro * pow(v, 2);
+        double p_0_rachet = pipe_dannye.p_L - pipe_dannye.L * (-4 / pipe_dannye.D * t_w - pipe_dannye.ro * M_G * (pipe_dannye.z_L - pipe_dannye.z_0) / pipe_dannye.L);
+        double delta_p_L;
+        
+
+            return
+        {
+
+           delta_p_L =  pipe_dannye.p_0- p_0_rachet
+
+        };
+    };
+};
+
+void u_Newton_EULER(struct pipe& myPipe) {
+
+    
+
+    // Подключение бибилиотеки
+// Класс, для системы размерности <2> - Векторный случай
+// <2> - Размерность системы уравнений
+
+
+    // Создание экземпляра класса, который и будет решаемой системой
+    QP_Newton_EULER test(myPipe);
+    // Задание настроек решателя по умолчанию
+    fixed_solver_parameters_t<1, 0> parameters;
+    // Создание структуры для записи результатов расчета
+    fixed_solver_result_t<1> result;
+    // Решение системы нелинейныйх уравнений <1> с помощью решателя Ньютона - Рафсона
+    // { 0, 0 } - Начальное приближение
+    fixed_newton_raphson<1>::solve_dense(test, {1}, parameters, &result);
+    cout << '\n' << "Классическая задача PP поверх Эйлера на методе Ньютона " << '\n' << "u = " << result.argument;
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
